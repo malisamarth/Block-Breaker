4fe68dd4d052774da764dd5c45efe5fdd4bfefe9
Samarth Kautik Mali <141520621+malisamarth@users.noreply.github.com>
2025-08-19 08:34:33 +0530

    Update BallScript.cs

--- BallScript.cs
+++ BallScript.cs
@@ -1,13 +1,41 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
     [SerializeField] Rigidbody2D ballRigid2d;
-    void Update()
-    {
-        if (transform.position.y < -5f) {
-            transform.position = new Vector2(0, 0);
-            ballRigid2d.linearVelocity = Vector2.zero;
+    public ScoreManagerScript scoreManagerScript;
+    private int maxLives = 3;
+    private float maxBallPositionY = -6f;
+    private bool isGameActive = true;
+    private void Start() {
+        scoreManagerScript = GameObject.FindWithTag("ScoreManager").GetComponent<ScoreManagerScript>();
+    }
+    void Update() { 
+
+        if (transform.position.y < maxBallPositionY && isGameActive) {
+            maxLives--;
+            scoreManagerScript.removeLive(maxLives);
+            if (maxLives == 0) {
+                //Debug.Log("gameover");
+                isGameActive = false;
+                ballRigid2d.gravityScale = 0;
+                ballRigid2d.linearVelocity = Vector2.zero;
+
+                scoreManagerScript.addCurrentScoreToHighest(scoreManagerScript.currentScore);
+                List<int> highestScoreArray = scoreManagerScript.GethighestScoreArray();
+                scoreManagerScript.addingScoreToOver(highestScoreArray);
+
+                scoreManagerScript.gameOver();
+            } else {
+                spwanBall();
+            }
         }
+
+    }
+
+    private void spwanBall() {
+        transform.position = new Vector2(0, -2.77f);
+        ballRigid2d.linearVelocity = Vector2.zero;
     }
 }
